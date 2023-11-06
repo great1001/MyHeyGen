c165cbfe3ce8dba98f34e5c862cd43ce2a33e5ac
AIFSH <149982694+AIFSH@users.noreply.github.com>
2023-11-06 12:42:40 +0800

    Update insall.sh

--- insall.sh
+++ insall.sh
@@ -1,5 +1,31 @@
 pip install pip -U
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install -r requirements.txt
+pip install TTS
+pip install onnxruntime
+pip install scenedetect
+pip install opencv-python
+pip install ultralytics
+pip install deepface
+pip install numpy==1.22.2
+pip install transformers
+pip install tensorflow==2.8.0
+pip install pydub
+pip install basicsr
+pip install facexlib
+pip install gfpgan
+pip install audiostretchy
+pip install numpy==1.22.2
+pip install ctranslate2
+pip install faster_whisper
+pip install ffmpeg_python==0.2.0
+pip install pyannote.audio
+pip install moviepy
+pip install cutlet
+pip install unidic-lite
+pip install face-alignment==1.3.4
+pip install ninja==1.10.2.3
+pip install dlib
+pip install librosa==0.9.2
+
 apt update
 apt install ffmpeg
